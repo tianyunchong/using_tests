7b1b76f577385186998e4de79e9d94b8644b6b1e
tianyuchong <854024109@qq.com>
2018-01-04 17:16:22 +0800

    增加下confd测试使用案例

---
+++ confd/etcd_write.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 写入配置文件到etcd
+ */
+use ActiveCollab\Etcd\Client as EtcdClient;
+require __DIR__ . '/vendor/autoload.php';
+$etcdConfig = array(
+    "host" => "127.0.0.1",
+    "port" => 2379,
+);
+$config = array(
+    '/gcv3/dev/sys/memcommon'  => array(
+        0 => ';公共memcached缓存集群',
+        1 => '[memcommon]',
+        2 => 'host = 192.168.8.189',
+        3 => 'port = 11211',
+    ),
+    '/gcv3/dev/sys/memtxy'     => array(
+        0 => ';腾讯云 暂搜索专用',
+        1 => '[memtxy]',
+        2 => 'host = 192.168.8.189',
+        3 => 'port = 11211',
+    ),
+    '/gcv3/dev/sys/specialpro' => array(
+        0 => ';specialpro 特权产品存储腾讯云memcached',
+        1 => '[specialpro]',
+        2 => 'host = 192.168.8.189',
+        3 => 'port = 11211',
+    ),
+);
+$client = new EtcdClient('http://' . $etcdConfig["host"] . ':' . $etcdConfig["port"]);
+//写入下Key值
+$client->set("/gcv3/dev/sys/conf_keys", "memcommon:memtxy:specialpro");
+//将配置信息写入etcd
+// foreach ($config as $key => $value) {
+//     foreach ($value as $key1 => $value1) {
+//         $client->set($key . "/" . $key1, $value1);
+//     }
+// }
+//获取目录下拥有的键值对
+// if ($client->dirExists("/gcv3/dev")) {
+//     var_dump($client->getKeyValueMap("/gcv3/dev"));
+// }
--- confd/readme.md
+++ confd/readme.md
@@ -2,8 +2,10 @@
 利用confd生成gcv3到指定机器生成配置目录文件
 
 1. php脚本写入配置数据到etcd
-https://github.com/linkorb/etcd-php
+	https://github.com/activecollab/etcd
 2. 建立confd配置模板和配置文件
 3. 预先建立gcv3配置目录结构
-3. 实现脚本转移confd配置模板和配置文件到配置位置
+3. 转移confd配置模板和配置文件到配置位置
 4. 启动confd,生成配置目录文件
+	confd -onetime -backend etcd -node http://127.0.0.1:2379
+	confd -interval=60 -backend etcd -node http://127.0.0.1:2379 &
